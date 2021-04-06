04fc14527e075812020b919111da02bdf956a700
Munch42 <munch4242@gmail.com>
2021-04-06 11:30:09 -0600

    Added Hands

---
+++ First VR Game/Assets/Scripts/HandPresence.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HandPresence : MonoBehaviour
+{
+    public InputDeviceCharacteristics controllerCharacteristics;
+    public List<GameObject> controllerPrefabs;
+    private InputDevice targetDevice;
+    private GameObject spawnedController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        //InputDeviceCharacteristics rightControllerCharacteristics = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
+
+        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
+
+        foreach(var item in devices)
+        {
+            Debug.Log(item.name + ": " + item.characteristics);
+        }
+
+        if(devices.Count > 0)
+        {
+            targetDevice = devices[0];
+            GameObject prefab = controllerPrefabs.Find(controller => controller.name == targetDevice.name);
+            if (prefab)
+            {
+                spawnedController = Instantiate(prefab, transform);
+            } else
+            {
+                Debug.Log("Did not find corresponding controller model!");
+                spawnedController = Instantiate(controllerPrefabs[0], transform);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue) && primaryButtonValue)
+        {
+            Debug.Log("Pressing Primary Button");
+        }
+
+        if(targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue) && triggerValue > 0.1f)
+        {
+            Debug.Log("Trigger Pressed: " + triggerValue);
+        }
+
+        if (targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2DAxisValue) && primary2DAxisValue != Vector2.zero)
+        {
+            Debug.Log("Primary Touchpad Pressed: " + primary2DAxisValue);
+        }
+    }
+}
